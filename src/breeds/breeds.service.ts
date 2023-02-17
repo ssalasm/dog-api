5b56d44eb0d6d31b777794a9d9de50c366e13dec
Sebastián Salas <100061338+ssalasm@users.noreply.github.com>
2023-02-16 22:27:30 -0300

    first commit

---
+++ src/breeds/breeds.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBreedDto } from './dto/create-breed.dto';
+import { UpdateBreedDto } from './dto/update-breed.dto';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Breed, BreedDocument } from 'src/schemas/breed.schema';
+
+@Injectable()
+export class BreedsService {
+
+  constructor(@InjectModel(Breed.name) private breedModel: Model<BreedDocument>) {}
+
+  async create(createBreedDto: CreateBreedDto): Promise<Breed> {
+    return new this.breedModel(createBreedDto).save();
+  }
+
+  findAll() {
+    return `This action returns all breeds`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} breed`;
+  }
+
+  update(id: number, updateBreedDto: UpdateBreedDto) {
+    return `This action updates a #${id} breed`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} breed`;
+  }
+}
